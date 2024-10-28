d14fbb43b6448ccc8dcf8bd6f0c1a8dafff76bd3
rohitmalekar <rohitmalekar@gmail.com>
2024-10-28 22:05:33 +0530

    Update gg22_grantsscope.py

--- gg22_grantsscope.py
+++ gg22_grantsscope.py
@@ -9,14 +9,15 @@ col1, col2, col3 = st.columns([0.25, 2, 0.25])  # Adjust the width ratios as nee
 def min_max_normalize(series):
     return (series - series.min()) / (series.max() - series.min())
 
+
 # List of metrics
 metrics = [
     "star_count",
     "fork_count",
-    "contributor_count_6_months",
-    "new_contributor_count_6_months",
-    "fulltime_developer_average_6_months",
-    "active_developer_count_6_months",
+#    "contributor_count_6_months",
+#    "new_contributor_count_6_months",
+#    "fulltime_developer_average_6_months",
+#    "active_developer_count_6_months",
     "commit_count_6_months",
     "merged_pull_request_count_6_months",
     "closed_issue_count_6_months"
@@ -33,6 +34,7 @@ col2.markdown(""" In GG22, we're taking it a step further. Donors can now create
 
 col2.link_button("Support GrantsScope in GG22", "https://explorer.gitcoin.co/#/round/42161/608/77", type="primary")
 scol1, scol2, scol3 = st.columns([1, 1.5, 1])  # Adjust the width ratios as needed
+scol2.markdown("**STEP 1:** Pick your metrics")
 selected_metrics = scol2.multiselect("Select up to 3 metrics:", metrics, max_selections=3)
 
 # Total weight variable
@@ -44,7 +46,7 @@ weights = {metric: 0 for metric in selected_metrics}
 
 # Input for weights using sliders
 if selected_metrics:
-    scol2.write("Assign weights (total must equal 100):")
+    scol2.markdown("**STEP 2:** Assign weights (total must equal 100):")
     remaining_weight = total_weight
 
     for metric in selected_metrics:
@@ -75,8 +77,8 @@ if selected_metrics:
         if scol2.button("Apply"):
             
             # Read the CSV files from the data folder
-            oso_df = pd.read_csv('./data/GG22 GrantsScope OSO - oso.csv')
-            regendata_df = pd.read_csv('./data/GG22 GrantsScope OSO - regendata.csv')
+            oso_df = pd.read_csv('./gg22/data/GG22 GrantsScope OSO - oso.csv')
+            regendata_df = pd.read_csv('./gg22/data/GG22 GrantsScope OSO - regendata.csv')
 
             # Remove the prefix "https://github.com/" from the project_github column
             regendata_df['oso_project_name'] = regendata_df['project_github'].str.replace("https://github.com/", "", regex=False)
@@ -95,6 +97,18 @@ if selected_metrics:
                 ) *100
             )
 
+            # Normalize the contributor count
+            merged_df['normalized_contributor_count_6_months'] = min_max_normalize(merged_df['contributor_count_6_months'])
+
+            # Calculate composite score per contributor
+            merged_df['composite_score_per_contributor'] = merged_df['composite_score'] / merged_df['normalized_contributor_count_6_months'].replace(0, 1)  # Replace 0 with 1 to avoid division by zero
+
+            # Normalize composite_score_per_contributor to a scale of 0 to 100
+            max_score_per_contributor = merged_df['composite_score_per_contributor'].max()
+            if max_score_per_contributor > 0:  # Avoid division by zero
+                merged_df['composite_score_per_contributor'] = round((merged_df['composite_score_per_contributor'] / max_score_per_contributor) * 100)
+            else:
+                merged_df['composite_score_per_contributor'] = 0  # Set to 0 if max is 0
             
             # Create a new DataFrame with the specified columns and rename them
             display_df = merged_df[[
@@ -102,6 +116,7 @@ if selected_metrics:
                 'round_name',
                 'explorer_link',
                 'composite_score',
+                'composite_score_per_contributor',
                 'star_count',
                 'fork_count',
                 'contributor_count_6_months',
@@ -116,6 +131,7 @@ if selected_metrics:
                 'round_name': 'Round',
                 'explorer_link': 'Donation Link',
                 'composite_score' : 'Composite Score (0 to 100)',
+                'composite_score_per_contributor' : 'Impact Efficiency Per Contributor',
                 'star_count': 'Star Count',
                 'fork_count': 'Fork Count',
                 'contributor_count_6_months': 'Contributor Count (6 Month)',
@@ -138,16 +154,34 @@ if selected_metrics:
             st.markdown("Below is a tailored ranking of GG22 projects based on the metrics you selected and the weights you assigned. The composite score, calculated on a scale from 0 to 100, reflects how each project aligns with your preferences. Explore the detailed data for each project, and if you find one that resonates with your goals, consider supporting it through a direct donation.")
             st.caption("Note: Projects that applied after the guarnteed review date might not showcase in this analysis.")
 
-            with st.expander("Understanding the Composite Score Calculation"):
-                st.markdown('''                            
+            with st.expander("Understanding the Composite Score and Impact Efficiency Per Contributor"):
+                st.markdown('''
+                    **Composite Score:**
                     The composite score is a key metric that helps us evaluate and compare different projects based on various performance indicators. Here’s a simple breakdown of how we calculate this score:
                     1. **Normalization of Metrics:** We start by normalizing each selected metric. Normalization is a process that adjusts the values of these metrics to a common scale, typically between 0 and 1. This ensures that no single metric disproportionately influences the overall score due to its scale.
                     2. **Weight Assignment**: Each metric is assigned a weight by the user based on its importance. You can think of weights as the significance of each metric in the overall evaluation. 
                     3. **Weighted Sum Calculation:** The composite score is calculated by taking the weighted sum of the normalized metrics. This means we multiply each normalized metric by its corresponding weight and then sum these values together. The multiplication by 100 scales the score to a percentage format, making it easier to interpret. The formula looks like this:
                     > Composite Score=(∑(Normalized Metric×Weight))×100
                     4. **Rounding:** Finally, we round the composite score to the nearest whole number for simplicity. This gives us a final score that is easy to read and understand.
+                            
+                    **Impact Efficiency Per Contributor:**
+
+                    This metric offers a detailed look at how efficiently each contributor drives impact in a project. Instead of simply looking at the overall size of a project’s team, this metric normalizes the project’s contributions by the number of contributors. This allows us to highlight how effective smaller or larger teams are at creating value.
+
+                    1. We first calculate a composite score, which takes into account key performance indicators such as commits, pull requests, and issues resolved.
+                    2. Then, we divide this composite score by the number of contributors in the last 6 months, adjusted so that projects with very few contributors aren’t disproportionately penalized.
+                    3. The result is a normalized score, scaled between 0 and 100, showing how much impact each contributor is responsible for, on average.
+                    
+                    **Why does this matter?**
+
+                    - **Fair Comparison:** By focusing on per-contributor efficiency, we ensure that smaller teams with fewer contributors aren't overshadowed by larger teams simply due to their size.
+                    - **Productivity:** This metric emphasizes contributor productivity, showcasing teams where each individual is making significant contributions.
+                    In essence, Impact Efficiency Per Contributor helps donors and users identify which projects are maximizing their contributors' potential, allowing for a more meaningful comparison of teams regardless of their size.
                     ''')
 
+            st.error("Note that the composite scores are relative within this group of projects, not absolute measures. A low score doesn't necessarily mean a project is inactive; it might be in a stable phase or have a different development model. \
+                    A higher Impact Efficiency Per Contributor indicates that a project is achieving more activity with fewer contributors. However, it's important to consider this alongside other factors such as project complexity, stage of development, and specific project goals, as high activity per contributor isn't always indicative of overall project health or success.")
+            
             st.dataframe(
                 display_df,
                 column_config={
@@ -156,9 +190,25 @@ if selected_metrics:
                         help="Click to donate",
                         display_text="Donate Here"
                     ),
+                    "Composite Score (0 to 100)": st.column_config.ProgressColumn(
+                        "Composite Score (0 to 100)",
+                        help="Composite score representing the overall impact and performance",
+                        format="%f",
+                        min_value=0,
+                        max_value=100
+                    ),
+                    "Impact Efficiency Per Contributor": st.column_config.ProgressColumn(
+                        "Impact Efficiency Per Contributor",
+                        help="The efficiency of impact per contributor, scaled from 0 to 100",
+                        format="%f",
+                        min_value=0,
+                        max_value=100
+                    ),
                 },
                 hide_index=True,
+                height=2000
             )
+
             
             
 
@@ -196,5 +246,3 @@ if selected_metrics:
             #            hide_index=True,
             #        )
             #
-            
-
